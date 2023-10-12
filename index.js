8f27775389751bf97f3b4b506fe457cd3b08927d
EzmOdeus <shafrax500@gmail.com>
2023-10-12 03:20:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ app.use(express.json());
 app.get("/vid", (req, res) => {
   res.json([
     {
-      " url": "vi",
+      " url": "https://media.islamway.net/lessons/582/452_Samer_AsmMohemen.mp4",
     },
     {
-      " url": "vi",
+      " url": "https://media.islamway.net/lessons/582/506_Samer_Qaynqi3.mp4",
     },
   ]);
 });
